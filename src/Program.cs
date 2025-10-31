2952572b7fdd2e6ff40d4302628e14c965d8423c
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2025-10-31 00:36:53 -0300

    Desafio completo

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,11 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
 
 app.Run();
